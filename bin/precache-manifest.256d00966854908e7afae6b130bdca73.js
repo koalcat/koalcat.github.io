ae472ce56ee769e42e11ad50e5ea4d25542ee463
xuchdeid <xuchdeid@gmail.com>
2020-09-06 21:43:47 +0800

    add bin

---
+++ bin/precache-manifest.256d00966854908e7afae6b130bdca73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22a147b6fc529fff696fd6f4812a858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f079afa7a96ce076d9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "182285c63da3474beb78",
+    "url": "/static/js/2.d823c647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d823c647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f079afa7a96ce076d9",
+    "url": "/static/js/main.8597ef58.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
